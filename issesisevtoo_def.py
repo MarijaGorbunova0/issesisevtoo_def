c8f4d92f045761606e1d3cb440ba1e5aced1f9b8
MarijaGorbunova0 <153904965+MarijaGorbunova0@users.noreply.github.com>
2024-03-07 15:52:11 +0200

    Update issesisevtoo_def.py

--- issesisevtoo_def.py
+++ issesisevtoo_def.py
@@ -1,7 +1,13 @@
 from modul import *
 kasutajad = []
 paroolid = []
-menu = input(" registreerimine - 1\n autoriseerimine - 2\n nime v�i parooli muutmine - 3\n ")
+menu = input(" registreerimine - 1\n autoriseerimine - 2\n nime või parooli muutmine - 3\n ")
 if menu == 1:
     nimi = input("registreerimine: kirjutage oma nimi")
     parool = input("registreerimine: kirjutage parool")
+    zaregistr = registr(nimi, parool, kasutajad, paroolid)
+    print(kasutajad)    
+elif menu == 2:
+    autoris(kasutajad,paroolid)
+    if nimi in polzovateli
+    menu2 = int(input("1 - nime või parooli muutmine "))
